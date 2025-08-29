8a69c13dbcd589acd41e15201eee18b42b580775
gsk <guanshoukui@zhito.com>
2025-08-29 16:28:48 +0800

    1. 添加独立推理脚本 2. 修正在cuda11.8下面的环境修正

--- setup.py
+++ setup.py
@@ -1,92 +1,19 @@
 import glob
 import os
-import re
 from setuptools import find_packages, setup
 from torch.utils.cpp_extension import CUDAExtension, BuildExtension
 
-
-def parse_requirements(fname='requirements.txt', with_version=True):
-    """Parse the package dependencies listed in a requirements file but strips
-    specific versioning information.
-    Args:
-        fname (str): path to requirements file
-        with_version (bool, default=False): if True include version specs
-    Returns:
-        List[str]: list of requirements items
-    CommandLine:
-        python -c "import setup; print(setup.parse_requirements())"
-    """
-    import sys
-    from os.path import exists
-    require_fpath = fname
-
-    def parse_line(line):
-        """Parse information from a line in a requirements text file."""
-        if line.startswith('-r '):
-            # Allow specifying requirements in other files
-            target = line.split(' ')[1]
-            for info in parse_require_file(target):
-                yield info
-        else:
-            info = {'line': line}
-            if line.startswith('-e '):
-                info['package'] = line.split('#egg=')[1]
-            else:
-                # Remove versioning from the package
-                pat = '(' + '|'.join(['>=', '==', '>']) + ')'
-                parts = re.split(pat, line, maxsplit=1)
-                parts = [p.strip() for p in parts]
-
-                info['package'] = parts[0]
-                if len(parts) > 1:
-                    op, rest = parts[1:]
-                    if ';' in rest:
-                        # Handle platform specific dependencies
-                        # http://setuptools.readthedocs.io/en/latest/setuptools.html#declaring-platform-specific-dependencies
-                        version, platform_deps = map(str.strip,
-                                                     rest.split(';'))
-                        info['platform_deps'] = platform_deps
-                    else:
-                        version = rest  # NOQA
-                    info['version'] = (op, version)
-            yield info
-
-    def parse_require_file(fpath):
-        with open(fpath, 'r') as f:
-            for line in f.readlines():
-                line = line.strip()
-                if line and not line.startswith('#'):
-                    for info in parse_line(line):
-                        yield info
-
-    def gen_packages_items():
-        if exists(require_fpath):
-            for info in parse_require_file(require_fpath):
-                parts = [info['package']]
-                if with_version and 'version' in info:
-                    parts.extend(info['version'])
-                if not sys.version.startswith('3.4'):
-                    # apparently package_deps are broken in 3.4
-                    platform_deps = info.get('platform_deps')
-                    if platform_deps is not None:
-                        parts.append(';' + platform_deps)
-                item = ''.join(parts)
-                yield item
-
-    packages = list(gen_packages_items())
-    return packages
-
-
-install_requires = parse_requirements()
-
+# 整个 parse_requirements 函数都可以被安全地删除，因为它没有被调用
 
 def get_extensions():
     extensions = []
 
+    # 找到所有需要编译的 C++/CUDA 源文件
     op_files = glob.glob('./clrnet/ops/csrc/*.c*')
     extension = CUDAExtension
     ext_name = 'clrnet.ops.nms_impl'
 
+    # 定义扩展模块
     ext_ops = extension(
         name=ext_name,
         sources=op_files,
@@ -108,9 +35,10 @@ setup(name='clrnet',
       ],
       packages=find_packages(),
       include_package_data=True,
-      setup_requires=['pytest-runner'],
-      tests_require=['pytest'],
-      install_requires=install_requires,
+      # 确认 install_requires 这一行不存在或被注释掉
+      # install_requires=... , 
+      
+      # 以下是编译扩展的核心部分
       ext_modules=get_extensions(),
       cmdclass={'build_ext': BuildExtension},
       zip_safe=False)
